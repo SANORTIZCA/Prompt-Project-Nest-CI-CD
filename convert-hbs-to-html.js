48c7607ec9ffaf525fcf3d588dc24b065144e609
Santiago Ortiz Cardona <sanortizca@gmail.com>
2024-11-17 13:42:17 -0500

    Views updated

--- convert-hbs-to-html.js
+++ convert-hbs-to-html.js
@@ -2,24 +2,39 @@ const fs = require('fs');
 const path = require('path');
 const hbs = require('hbs');
 
-// Ruta al archivo home.hbs
-const hbsFilePath = path.join(__dirname, 'views', 'home.hbs');
-// Ruta al archivo index.html de salida
-const htmlFilePath = path.join(__dirname, 'dist', 'index.html');
+// Ruta al directorio de vistas
+const viewsDir = path.join(__dirname, 'views');
+// Ruta al directorio de salida
+const distDir = path.join(__dirname, 'dist', 'views');
 
-// Leer el archivo home.hbs
-const hbsContent = fs.readFileSync(hbsFilePath, 'utf-8');
-// Compilar el contenido de Handlebars
-const template = hbs.handlebars.compile(hbsContent);
-// Generar el contenido HTML
-const htmlContent = template({});
-
-// Crear el directorio dist si no existe
-if (!fs.existsSync(path.join(__dirname, 'dist'))) {
-  fs.mkdirSync(path.join(__dirname, 'dist'));
+// Crear el directorio dist/views si no existe
+if (!fs.existsSync(distDir)) {
+  fs.mkdirSync(distDir, { recursive: true });
 }
 
-// Escribir el contenido HTML en index.html
-fs.writeFileSync(htmlFilePath, htmlContent);
+// Función para convertir un archivo HBS a HTML
+const convertHbsToHtml = (hbsFilePath) => {
+  const fileName = path.basename(hbsFilePath, '.hbs');
+  const htmlFilePath = path.join(distDir, `${fileName}.html`);
 
-console.log('home.hbs convertido a index.html');
+  // Leer el archivo HBS
+  const hbsContent = fs.readFileSync(hbsFilePath, 'utf-8');
+  // Compilar el contenido de Handlebars
+  const template = hbs.handlebars.compile(hbsContent);
+  // Generar el contenido HTML
+  const htmlContent = template({});
+
+  // Escribir el contenido HTML en el archivo de salida
+  fs.writeFileSync(htmlFilePath, htmlContent);
+  console.log(`${hbsFilePath} convertido a ${htmlFilePath}`);
+};
+
+// Leer todos los archivos HBS en el directorio de vistas
+fs.readdirSync(viewsDir).forEach((file) => {
+  if (path.extname(file) === '.hbs') {
+    const hbsFilePath = path.join(viewsDir, file);
+    convertHbsToHtml(hbsFilePath);
+  }
+});
+
+console.log('Todas las vistas han sido convertidas a HTML');
